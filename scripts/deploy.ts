a19a6ca4efd6d3aa0a67f3acd157403322f6d7b2
Nedko Dimitrov <nd.nekko@gmail.com>
2023-05-20 17:30:19 +0300

    Convert to hardhat

---
+++ scripts/deploy.ts
@@ -0,0 +1,15 @@
+import { ethers } from "hardhat"
+
+async function main() {
+  const BookLibrary = await ethers.getContractFactory("BookLibrary")
+  const bookLibrary = await BookLibrary.deploy()
+
+  await bookLibrary.deployed()
+
+  console.log("BookLibrary deployed to:", bookLibrary.address)
+}
+
+main().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
